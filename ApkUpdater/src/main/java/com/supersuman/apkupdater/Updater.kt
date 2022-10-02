3c11ca0fee0acf76d9acc2a9806659e27d5f69ef
Sumanth Perambuduri <35676632+supersu-man@users.noreply.github.com>
2022-10-02 22:28:33 +0530

    refactor: removed unnecessary permissions

--- ApkUpdater/src/main/java/com/supersuman/apkupdater/Updater.kt
+++ ApkUpdater/src/main/java/com/supersuman/apkupdater/Updater.kt
@@ -1,19 +1,10 @@
 package com.supersuman.apkupdater
 
-import android.Manifest
 import android.app.Activity
 import android.app.DownloadManager
 import android.content.Context
-import android.content.pm.PackageManager
 import android.net.Uri
-import android.os.Build
 import android.os.Environment
-import com.karumi.dexter.Dexter
-import com.karumi.dexter.PermissionToken
-import com.karumi.dexter.listener.PermissionDeniedResponse
-import com.karumi.dexter.listener.PermissionGrantedResponse
-import com.karumi.dexter.listener.PermissionRequest
-import com.karumi.dexter.listener.single.PermissionListener
 import khttp.get
 import kotlin.concurrent.thread
 
@@ -33,7 +24,6 @@ class ApkUpdater(private val activity: Activity, url: String) {
         return null
     }
 
-
     fun requestDownload() {
         val src = Regex("\"http.+?expanded.+?\"").find(response)?.value?.removeSurrounding("\"") ?: return
         val res = get(src).text
@@ -45,34 +35,6 @@ class ApkUpdater(private val activity: Activity, url: String) {
         request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
         request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, title)
         downloadManager.enqueue(request)
-    }
-
-    fun requestMyPermissions(function: () -> Unit) {
-        Dexter.withContext(activity).withPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
-            .withListener(object : PermissionListener {
-                override fun onPermissionGranted(response: PermissionGrantedResponse) {
-                    function()
-                }
-
-                override fun onPermissionDenied(response: PermissionDeniedResponse) {
-                    println(response)
-                }
-
-                override fun onPermissionRationaleShouldBeShown(
-                    permission: PermissionRequest?, token: PermissionToken?
-                ) {
-                    println(permission)
-                    println(token)
-                }
-            }).check()
-    }
-
-    fun hasPermissionsGranted(): Boolean {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            return activity.checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED
-        } else {
-            return true
-        }
     }
 
     fun isInternetConnection(): Boolean {
